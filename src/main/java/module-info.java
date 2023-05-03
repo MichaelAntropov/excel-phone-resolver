066f81e5eb61e63b50b92d1388b4a2ae1aa4e175
Michael Antropov <mikhael.antropov@gmail.com>
2023-05-03 12:12:28 +0300

    Update poi-ooxml to 5.2.3

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module ExcelPhoneResolver {
     requires javafx.fxml;
     requires javafx.controls;
-    requires poi;
-    requires poi.ooxml;
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
 
     opens com.hizencode.javafx.controller to javafx.fxml;
     opens com.hizencode.javafx.main to javafx.graphics;
