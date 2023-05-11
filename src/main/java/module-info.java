59d7800dfa3901fc6baa4ddea9db25a40485b0d2
Michael Antropov <mikhael.antropov@gmail.com>
2023-05-11 21:19:55 +0300

    Remove legacy resolve menu

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,5 @@ module ExcelPhoneResolver {
 
     opens com.hizencode.excelphoneresolver.main to javafx.graphics;
     opens com.hizencode.excelphoneresolver.ui.mainscene.controller to javafx.fxml;
-    opens com.hizencode.excelphoneresolver.ui.start.controller to javafx.fxml;
     opens com.hizencode.excelphoneresolver.ui.alertmanager to javafx.fxml;
-    opens com.hizencode.excelphoneresolver.ui.resolve.controller to javafx.fxml;
 }
