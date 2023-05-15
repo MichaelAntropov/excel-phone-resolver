0d2acc16b24724b4edb44d85b16c1fb695f6a236
Michael Antropov <mikhael.antropov@gmail.com>
2023-05-15 18:54:29 +0300

    Add Nord dark and light themes

---
+++ src/main/java/com/hizencode/excelphoneresolver/ui/theme/Theme.java
@@ -0,0 +1,23 @@
+package com.hizencode.excelphoneresolver.ui.theme;
+
+public enum Theme {
+    LIGHT("NORD_LIGHT", "/css/nord-light.css"),
+    DARK("NORD_DARK","/css/nord-dark.css");
+
+    private final String name;
+
+    private final String cssPath;
+
+    Theme(String name, String cssPath) {
+        this.name = name;
+        this.cssPath = cssPath;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCssPath() {
+        return cssPath;
+    }
+}
