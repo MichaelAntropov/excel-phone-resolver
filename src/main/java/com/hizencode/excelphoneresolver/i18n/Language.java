db6903782ab648064a0687f08589a6bd6c044d45
Michael Antropov <mikhael.antropov@gmail.com>
2023-05-16 21:05:23 +0300

    Move field in Language

--- src/main/java/com/hizencode/excelphoneresolver/i18n/Language.java
+++ src/main/java/com/hizencode/excelphoneresolver/i18n/Language.java
@@ -7,11 +7,11 @@ public enum Language {
     ENGLISH("English", new Locale("en")),
     UKRAINIAN("Українська", new Locale("uk"));
 
+    public static final Language DEFAULT_LANGUAGE = Language.ENGLISH;
+
     private final String langName;
 
     private final Locale locale;
-
-    public static final Language DEFAULT_LANGUAGE = Language.ENGLISH;
 
     Language(String langName, Locale locale) {
         this.langName = langName;
